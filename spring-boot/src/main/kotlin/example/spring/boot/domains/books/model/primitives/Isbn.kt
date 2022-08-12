ec136869954670f56d08c9dcdde96b8dac237f0c
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2022-08-12 12:29:13 +0200

    inline classes do not validate during de-serialization

--- spring-boot/src/main/kotlin/example/spring/boot/domains/books/model/primitives/Isbn.kt
+++ spring-boot/src/main/kotlin/example/spring/boot/domains/books/model/primitives/Isbn.kt
@@ -1,10 +1,15 @@
 package example.spring.boot.domains.books.model.primitives
 
-@JvmInline
-value class Isbn(val value: String) {
+import com.fasterxml.jackson.annotation.JsonValue
+
+data class Isbn(
+    @JsonValue private val value: String
+) {
     init {
         require(value matches Regex("""(\d{3}-)?\d{10}""")) {
             "[$value] is not a valid 10 or 13 digit ISBN!"
         }
     }
+
+    override fun toString() = value
 }
--- spring-boot/src/main/kotlin/example/spring/boot/domains/books/model/primitives/NumberOfPages.kt
+++ spring-boot/src/main/kotlin/example/spring/boot/domains/books/model/primitives/NumberOfPages.kt
@@ -1,12 +1,18 @@
 package example.spring.boot.domains.books.model.primitives
 
+import com.fasterxml.jackson.annotation.JsonValue
+
 private val validRange = 1..10_000
 
-@JvmInline
-value class NumberOfPages(val value: Int) {
+data class NumberOfPages(
+    @JsonValue private val value: Int
+) {
     init {
         require(value in validRange) {
             "[$value] is not a valid number of pages! (must be between ${validRange.first} and ${validRange.last})"
         }
     }
+
+    fun toInt(): Int = value
+    override fun toString() = value.toString()
 }
